4ce705a6e3bb24a8514d51d5c16b4b88c70e41ab
parke <parke.loyd@gmail.com>
2018-02-05 18:38:11 -0700

    commented powerlaw

--- powerlaw.py
+++ powerlaw.py
@@ -5,16 +5,48 @@ from scipy.optimize import minimize
 from scipy.special import gammaln
 from warnings import warn
 
-def loglike_from_interval(x, interval):
-    if x < interval[0] or x > interval[-1]:
-        return -np.inf
-    else:
-        return 0.0
+def loglike_from_interval(interval):
+    """
+    Create a function to represent a uniform likelihood across an interval, helpful for specifying a prior.
+
+    Parameters
+    ----------
+    interval : [lo, hi]
+        The lower and upper limits of the interval (exclusive). Can be -/+ np.inf.
+
+    Returns
+    -------
+    loglike : function
+        A function loglike(x) that gives 0.0 when x is in interval and -np.inf when it is not.
+
+    """
+    def loglike(x):
+        if x < interval[0] or x > interval[-1]:
+            return -np.inf
+        else:
+            return 0.0
+
+    return loglike
 
 
 def loglike_from_hist(bins, counts):
+    """
+    Create a function to represent a likelihood function based on histogrammed values.
+
+    Parameters
+    ----------
+    bins : bin edges used in the histogram.
+    counts : counts in each bin (histogram will be normalized such that integral is 1, though it shouldn't really matter)
+
+    Returns
+    -------
+    loglike : function
+        A function loglike(x) that returns ln(likelihood(x)) based on the value of the input histogram at x.
+    """
+
     integral = np.sum(np.diff(bins)*counts)
     norm = counts/integral
+
     def loglike(x):
         if x <= bins[0]:
             return -np.inf
@@ -22,15 +54,30 @@ def loglike_from_hist(bins, counts):
             return -np.inf
         i = np.searchsorted(bins, x)
         return np.log(norm[i - 1])
+
     return loglike
 
 
 def _prior_boilerplate(prior):
+    """
+    Standardize input for prior distributions.
+
+    Parameters
+    ----------
+    prior : None, [lo, hi] interval, or function
+        user input for prior
+
+    Returns
+    -------
+    loglike : function
+        Standardized loglikelihood function.
+    """
+
     if prior is None:
         return lambda x: 0.0
     elif not hasattr(prior, '__call__'):
         try:
-            return lambda x: loglike_from_interval(x, prior)
+            return loglike_from_interval(prior)
         except TypeError:
             raise ValueError('a_prior must either be a function or a list/tuple/array. See docstring.')
     else:
@@ -41,26 +88,31 @@ class PowerLawFit(object):
 
     def __init__(self, flare_dataset, a_prior=None, logC_prior=None, nwalkers=10, nsteps=1000):
         """
-        Create a PowerLawMCMC object. Intended to be created by a call to Flares.mcmc_powerlaw.
-        """
-        if flare_dataset.n_total < 3 and a_prior is None:
-            raise ValueError('At least 3 flares required to attempt a fit unless you place a prior on a. '
-                             'Several more than 3 will likely be required for said fit to converge.')
-        # if flare_dataset.n_total == 0:
-        #     raise ValueError('No flares in the provided FlareDataset.')
+        Create an object representing a power-law fit to the flare frequency distribution for the flare observations
+        provided in a FlareDataset object. The fit is of the form
 
+        f = C*e**-a,
+
+        where f is the cumulative frequency of flares with energies greater than e.
+        """
         self.flare_dataset = flare_dataset
         self.a_prior = _prior_boilerplate(a_prior)
         self.logC_prior = _prior_boilerplate(logC_prior)
         self.n = flare_dataset.n_total
+
+        if self.n < 3 and a_prior is None:
+            raise ValueError('At least 3 flares required to attempt a fit unless you place a prior on a. '
+                             'Several more than 3 will likely be required for said fit to converge.')
         if self.n == 0:
             warn('No flares in the flare dataset. Be cautious: for MCMC samples of the posterior of C and similar '
                  'derived quantities (such as flare rate), use only the linear values, not logC, and use them only to '
                  'constrain upper limits.')
 
-        # now make a function to rapidly compute the log likelihood.
-
-        # collect info from datasets into arrays for fast power law loglike computations
+        # region log likelihood function
+        #
+        # now make a function to rapidly compute the log likelihood. This would be much more readable if I separately
+        # computed the log likelihood for each flare observation object and added them. However, it is much faster if
+        # I aggregate the data from each and compute likelihoods "all at once."
 
         # first handle the power law portion of the likelihood
         expt, elim, n, e = [], [], [], []
@@ -80,13 +132,17 @@ class PowerLawFit(object):
         expt = [obs.expt for obs in self.flare_dataset.observations]
         Dexpt, Delim, Dn = map(np.array, [expt, elim, n])
         def loglike_poisson(a_cum, logC):
-            # keeping lambda in log space avoids log(lam = 0) resulting from some MCMC samples where very low logC
-            # values are tried such that 10**logC = 0 to computer precision
+            # sometimes the MCMC sampler tries values for logC that are so low that 10**logC = 0 to computer precision
+            # keeping lambda in log space avoids this resulting in a nan likelihood resulting from a np.log(0) operation
             loglam = np.log(Dexpt) + logC*np.log(10.) - a_cum*np.log(Delim)
             return np.sum(-10**loglam + Dn * loglam - gammaln(Dn + 1))
 
         # all together now!
-        # keep C in log space. Otherwise, it seems the MCMC can't handle the narrow, highly curved distribution.
+        # Define likelihood as a function of a,logC so that the MCMC sampler will explore that space.
+        # This avoids problems that occurred when the MCMC sampler tried to explore a,C space. The distribution in that
+        # space is very narrow and highly curved, and it seems MCMC just can't accurately feel its way around it.
+        #
+        # However, an exception is when there are no flares to constrain the flare rate. We will get to that.
         def loglike(params):
             """
             Log likelihood of the posterior distribution for values a and logC for a power-law fit to the cumulative
@@ -112,8 +168,13 @@ class PowerLawFit(object):
             return (loglike_powerlaw(a_cum + 1) + loglike_poisson(a_cum, logC) + self.a_prior(a_cum)
                     + self.logC_prior(logC))
 
+        # save the loglike function for user access, if desired.
         self.loglike = loglike
+        # endregion
 
+        # region sample the fit posterior
+
+        # make a function for guessing appropriate rate constants given values of a
         elim_mean = np.mean([data.elim for data in self.flare_dataset.observations])
         def guess_logC(a_guess):
             n = self.flare_dataset.n_total
@@ -125,10 +186,9 @@ class PowerLawFit(object):
         # flares and it is specified as logC (log10(C)). I think this is because it is basically unconstrained to -inf.
         # However, when there is a flare logC tends to be much more normally distributed than C and if C is used the
         # MCMC sampler does wacky things. So I'll treat the two separately.
+        if flare_dataset.n_total > 0: # sample in a,logC space
 
-        if flare_dataset.n_total > 0:
-
-            # try to find max likelihood values
+            # try to find max-likelihood values of a, logC using numerical minimization of -loglike
             def neglike(params):
                 return -loglike(params)
             a_guess = self._quick_n_dirty_index()
@@ -136,6 +196,8 @@ class PowerLawFit(object):
                 a_guess = 1.0
             logC_guess = guess_logC(a_guess)
             result = minimize(neglike, [a_guess, logC_guess], method='Nelder-Mead')
+
+            # store the max-likelihood values
             self.ml_result = result
             if not result.success or not np.all(np.isfinite(result.x)):
                 self.ml_success = False
@@ -149,39 +211,48 @@ class PowerLawFit(object):
                 self.logC_ml = logC
                 a_init = a
 
+            # get ready to MCMC sample the posterior
             pos = []
             for _ in range(nwalkers):
                 a = a_init + np.random.normal(0, 0.01)
                 logC = guess_logC(a_init) + np.random.normal(0, 0.2)
                 pos.append([a, logC])
-
             sampler = emcee.EnsembleSampler(nwalkers, 2, loglike)
 
-        else: # no flares in dataset
+        else: # no flares in dataset, sample in a,C space
+
+            # no max-like fit is possible in this case
             self.ml_success = False
             self.a_ml = None
             self.logC_ml = None
             a_init = 1.0
             pos = []
 
+            # make a loglike function that allows sampling of a,C instead of a,logC
             def loglike_linear(params):
                 a, C = params
                 if C < 0:
                     return -np.inf
                 return loglike([a, np.log10(C)])
 
+            # get ready to MCMC sample the posterior of a,C
             for _ in range(nwalkers):
                 a = a_init + np.random.normal(0, 0.01)
                 logC = guess_logC(a_init) + np.random.normal(0, 0.2)
                 C = 10**logC
                 pos.append([a, C])
-
             sampler = emcee.EnsembleSampler(nwalkers, 2, loglike_linear)
 
+        # run some throw-away burn-in MCMC steps
         pos, prob, state = sampler.run_mcmc(pos, 100) # burn in
         sampler.reset()
+
+        # MCMC sample either a,logC or a,C
         sampler.run_mcmc(pos, nsteps)
+
+        # save the sampler, but don't let the user see it because the logC vs C possibility will definitely lead to confusion
         self._MCMCsampler = sampler
+        # endregion
 
     @property
     def a(self):
@@ -202,6 +273,17 @@ class PowerLawFit(object):
 
 
     def run_mcmc(self, nsteps):
+        """
+        Advance the MCMC sampling of the fit posterior by nsteps.
+        Parameters
+        ----------
+        nsteps : int
+            number of MCMC steps to take
+
+        Returns
+        -------
+
+        """
         self._MCMCsampler.run_mcmc(None, nsteps)
 
 
@@ -215,8 +297,8 @@ class PowerLawFit(object):
 
         Returns
         -------
-        a, aerr: floats
-            max likelihood power-law index and error
+        a : float
+            max likelihood power-law index
         """
         e = np.concatenate([data.e for data in self.flare_dataset.observations])
         elim = np.concatenate([[data.elim] * data.n for data in self.flare_dataset.observations])
@@ -225,6 +307,19 @@ class PowerLawFit(object):
         return a
 
     def plotfit(self, ax=None, line_kws=None, step_kws=None):
+        """
+        Plot a the corrected cumulative distribution of flares and the power-law max-likelihood fit.
+
+        Parameters
+        ----------
+        ax : axes on which to draw plot
+        line_kws : dict of keywords to pass on to plot of best-fit line
+        step_kws : dict of keywords to pass on to step plot of observed flare cumulative frequencies
+
+        Returns
+        -------
+
+        """
         if self.ml_success == False:
             raise ValueError('No best fit to plot for this Fit object.')
         if ax is None:
@@ -232,7 +327,7 @@ class PowerLawFit(object):
         self.flare_dataset.plot_ffd(ax=ax, **step_kws)
         emin = min(*[d.elim for d in self.flare_dataset.observations])
         emax = max(*[np.max(d.e) for d in self.flare_dataset.observations])
-        plot(self.a_ml, self.C_ml, emin, emax, **line_kws)
+        plot(self.a_ml, 10**self.C_ml, emin, emax, **line_kws)
 
 
 def cumulative_frequency(a, C, e):
@@ -289,6 +384,8 @@ def plot(a, C, emin, emax, *args, **kwargs):
     C : float
     emin : float
     emax : float
+    ax : axes object
+        axes on which to plot the line
     args :
         passed to matplotlib plot function
     kwargs :
