9a8b88066542eb5f5dd072786bd330a31e1c10d0
parke <parke.loyd@gmail.com>
2018-04-14 00:04:10 -0700

    tweaks

--- tests.py
+++ tests.py
@@ -8,15 +8,17 @@ def simple_ffd(a_true=1., emin=1., emax=100., n_events=10, n_trials=1000):
 
     a = []
     for _ in range(n_trials):
-        e = powerlaw.random_energies()
+        e = powerlaw.random_energies(a_true, emin, emax, n_events)
         obs = ffd.Observation(emin, 1.0, e)
         set = ffd.FlareDataset([obs])
-        fit = powerlaw.PowerLawFit(set, nwalkers=1, nsteps=1)
+        fit = powerlaw.PowerLawFit(set, nwalkers=10, nsteps=1)
         a.append(fit.a_best)
 
     plt.figure()
     plt.xlabel('Retrieved a')
-    plt.hist(a, np.sqrt(n_trials))
+    plt.hist(a, int(np.sqrt(n_trials)))
     plt.axvline(a_true)
     plt.annotate('True Value', xy=(a_true, 0.05), xytext=(2,0), xycoords=('data', 'axes fraction'),
-                 textcoords='offset points', rotate=90, ha='left', va='bottom')
+                 textcoords='offset points', rotation=90, ha='left', va='bottom')
+    print '(Median - True)/(error on mean) = {:.2f}'.format((np.median(a) - a_true)/(np.std(a)/np.sqrt(n_trials)))
+    return a
